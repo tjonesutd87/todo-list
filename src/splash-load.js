d0aacba3599a1a68f3ee2657dd16a74921bbde3d
tjonesutd87 <tjonesutd87@gmail.com>
2025-01-15 07:51:23 -0600

    add logo, update other images

--- src/splash-load.js
+++ src/splash-load.js
@@ -1,29 +1,30 @@
 import horse from './images/horse-2795105_640.png';
 import logo from './images/todoerer-logo.png';
 import moon from './images/moon-4917183_640.png';
+import grass from './images/hq720.png';
 
 export function splashPageLoader() {
     //  declare variables to create page elements
     const splashContainer = document.createElement('div');
     const horseImg = document.createElement('img');
     const moonImg = document.createElement('img');
-    const logoBtn = document.createElement('button');
     const logoImg = document.createElement('img');
-    const grass = document.createElement('div');
+    const grassImg = document.createElement('img');
+    const text = document.createElement('p');
 
     document.body.appendChild(splashContainer);
-
+    splashContainer.appendChild(grassImg);
     splashContainer.appendChild(horseImg);
     splashContainer.appendChild(moonImg);
-    splashContainer.appendChild(logoBtn);
-    logoBtn.appendChild(logoImg);
-    splashContainer.appendChild(grass);
+    splashContainer.appendChild(logoImg);
+    splashContainer.appendChild(text);
+
 
     //  set element image sources and ids
     splashContainer.id = 'splash-container';
     horseImg.src = horse;
     horseImg.id = 'horse';
-    horseImg.width = 300;
+    horseImg.width = 150;
     moonImg.src = moon;
     moonImg.id = 'moon';
     moonImg.width= 300;
@@ -31,8 +32,9 @@ export function splashPageLoader() {
     logoImg.id = 'logo';
     logoImg.width = 600;
     logoImg.height = 457;
-    grass.id = 'grass';
-
-
+    grassImg.src = grass;
+    grassImg.id = 'grass';
+    text.textContent = 'Press A to Start';
+    text.id = 'start-text';
 
 }
